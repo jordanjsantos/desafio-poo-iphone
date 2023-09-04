7ffebc5a138e090ab993b302e4c07389e21ae401
Jordan Santos <jrdnsantos@gmail.com>
2023-09-04 19:54:07 -0300

    Criando objetos e chamado os metodos

--- src/me/dio/Main.java
+++ src/me/dio/Main.java
@@ -1,7 +1,31 @@
 package me.dio;
 
+import me.dio.interfaces.AparelhoTelefonico;
+import me.dio.interfaces.NavegadorInternet;
+import me.dio.interfaces.ReprodutorMusical;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        Dispositivo dispositivo = new Dispositivo();
+
+        ReprodutorMusical reprodutorMusical = dispositivo;
+        AparelhoTelefonico aparelhoTelefonico = dispositivo;
+        NavegadorInternet navegadorInternet = dispositivo;
+
+        System.out.println("Executando ações referentes ao reprodutor musical");
+        reprodutorMusical.tocar();
+        reprodutorMusical.pausar();
+        reprodutorMusical.selecionarMusica();
+
+        System.out.println("\n" + "Executando ações referentes ao aparelho telefônico");
+        aparelhoTelefonico.ligar();
+        aparelhoTelefonico.atender();
+        aparelhoTelefonico.iniciarCorreioVoz();
+
+        System.out.println("\n" + "Executando ações referentes ao navegador de internet");
+        navegadorInternet.exibirPagina();
+        navegadorInternet.adicionarNovaAba();
+        navegadorInternet.atualizarPagina();
     }
 }
